51f2ceefd385f1c9cd61915d73c65cab32f87b6f
EC2 Default User <ec2-user@ip-172-31-18-114.us-west-2.compute.internal>
2024-11-27 07:48:58 +0000

    change autoscaler file

---
+++ cluster-autoscaler.tf
@@ -0,0 +1,8 @@
+module "eks-cluster-autoscaler" {
+  source = "lablabs/eks-cluster-autoscalar/aws"
+  version="2.2.0"
+  #insert the 3 required variables here
+  cluster_name                     = module.eks.cluster_name
+  cluster_identity_oidc_issuer     = module.eks.oidc_provier
+  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+}
