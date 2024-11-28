8c0848fe5d0fdf3cb1ec2fe0b13bfd7ed86706a8
EC2 Default User <ec2-user@ip-172-31-18-114.us-west-2.compute.internal>
2024-11-28 14:15:27 +0000

    change autoscaler

--- cluster-autoscaler.tf
+++ cluster-autoscaler.tf
@@ -3,6 +3,6 @@
   version="2.2.0"
   #insert the 3 required variables here
   cluster_name                     = module.eks.cluster_name
-  cluster_identity_oidc_issuer     = module.eks.oidc_provier
+  cluster_identity_oidc_issuer     = module.eks.oidc_provider
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
 }
