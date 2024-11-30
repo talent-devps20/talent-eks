e66631da1be39a8c4e7cca756a48ca7264928e0d
Sumalatha S <suma83.s@gmail.com>
2024-11-30 17:32:37 +0530

    Delete external-dns.tf

--- external-dns.tf
+++
@@ -1,7 +0,0 @@
-module "eks-external-dns" {
-  source  = "lablabs/eks-external-dns/aws"
-  version = "1.2.0"
-  # insert the 2 required variables here
-  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
-  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
-}
